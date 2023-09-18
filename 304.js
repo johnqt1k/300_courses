fcc99ead4a6d7dce3b864f93bbd61be0b8f8f245
johnqt1k <john.sehlstrom@edu.huddinge.se>
2023-09-18 10:12:15 +0200

    added code for course

--- 304.js
+++ 304.js
@@ -1,1 +1,5 @@
+const str1 = "Hello world";
+const str2 = "Hell";
 
+console.log(`The word "${str2}" ${str1.includes(str2) ? 'is' : 'is not'} in the sentence`);
+console.log(str1.startsWith('Hell'));
