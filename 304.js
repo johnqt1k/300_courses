ed54c6e9e0799ff8fe35d317e845f7a8641d4539
johnqt1k <john.sehlstrom@edu.huddinge.se>
2023-09-18 10:37:29 +0200

    added code for course

--- 304.js
+++ 304.js
@@ -1,5 +1,20 @@
-const str1 = "Hello world";
-const str2 = "Hell";
+function findTarget (arr, target) {
+    const index = arr.findIndex(element => element > target);
 
-console.log(`The word "${str2}" ${str1.includes(str2) ? 'is' : 'is not'} in the sentence`);
-console.log(str1.startsWith('Hell'));
+    if (index !== -1) {
+        return {
+            value: arr [index],
+            Index: index
+        };
+    } else {
+        return null;
+    }
+}
+
+const numbers = [1, 4, 7, 10 ,15];
+const targetNumber = 8;
+
+const result = findTarget (numbers, targetNumber);
+
+console.log(`Value: ${result.value}, Index: ${result.Index}`);
+
